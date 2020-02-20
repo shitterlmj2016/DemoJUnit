f323738cef38dde1d16ae41086794c875114f6e7
xinchenh <xinchenh@amazon.com>
2020-02-19 17:12:46 -0800

    Update

--- src/main/java/com/xinchenh/junit/Calculator.java
+++ src/main/java/com/xinchenh/junit/Calculator.java
@@ -2,7 +2,16 @@ package com.xinchenh.junit;
 
 public class Calculator {
     CalculatorService service;
-    public int add(int i, int j){
-        return service.add(i, j);
+
+    public Calculator(CalculatorService service){
+        this.service = service;
+    }
+
+    public int perform(int i, int j){ //(i + j) * 2
+        return service.add(i, j) * 2;
+    }
+
+    public int hardCodedPerform(int i, int j) {
+        return (i + j) * 2;
     }
 }
--- src/test/java/com/xinchenh/junit/TestCalculator.java
+++ src/test/java/com/xinchenh/junit/TestCalculator.java
@@ -2,23 +2,52 @@ package com.xinchenh.junit;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class TestCalculator {
 
     Calculator c;
+    //Mock 一个service
+    //CalculatorService service = Mockito.mock(CalculatorService.class)；
+
+    //替代方法
+    @Mock
+    CalculatorService service;
+
+    //声明搭配mockito使用Junit, 不然@mock会出错
+    @Rule public MockitoRule rule = MockitoJUnit.rule();
+
+
     //测试之前做的事情
     @Before
-    public void setUp(){
-        c = new Calculator();
-    }
-    @Test
-    public void testAdd(){
-        assertEquals(5,c.add(2,3));
+    public void setUp() {
+        c = new Calculator(service);
     }
 
+    @Test
+    public void testPerform(){
+        when(service.add(2, 3)).thenReturn(5);
+        assertEquals(10,c.perform(2,3));
+    }
+
+    @Test
+    public void testHardCodedPerform(){
+        when(service.add(2, 3)).thenReturn(5);
+        assertEquals(10,c.hardCodedPerform(2,3));
+        //verify if the service has been called
+        verify(service).add(2,3);
+    }
+
+    //测试之后做的事
     @After
     public void after(){
         c = null;
